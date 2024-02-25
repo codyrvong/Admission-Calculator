251f4a20f37e80a492d4ba024aaa4de4ce680b88
Cody Vong <me@example.com>
2024-02-24 20:18:37 -0800

    Add project files.

---
+++ Admission/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using static System.Console;
+class Admission
+{
+    static void Main()
+    {
+        double GPA;
+        string GPAString;
+        Write("Enter your GPA >> ");
+        GPAString = ReadLine();
+        GPA = Convert.ToDouble(GPAString);
+        double testscore;
+        string score;
+        Write("Enter your test score >> ");
+        score = ReadLine();
+        testscore = Convert.ToDouble(score);
+        if (GPA >= 3.6 && testscore >= 80)
+            WriteLine("You are accepted");
+        else if (GPA <= 3.6 && testscore >= 90)
+            WriteLine("You are accepted");
+        else
+            WriteLine("You are rejected");
+    }
+}
